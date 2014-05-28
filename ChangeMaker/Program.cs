b4348ce2207d6cc6167cc27b3e6644119356d3b0
SeedPaths-User <lessdismissive@email.com>
2014-05-28 10:27:06 -0600

    Initial Check-in
    Check in to confirm upload. Program works well, will update for user
    input via Console.Readline(). This upload was needed since current
    laptop is being returned soon

---
+++ ChangeMaker/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChangeMaker(4.17);
+            Console.ReadKey();
+        }
+        static void ChangeMaker(double amount)
+        {
+            // Function will take an amount and divide into amount in coins.
+
+            var quarter = 0;//how many quarters
+            var dime = 0;//how many dimes
+            var nickle = 0;//how many nickles
+            var penney = 0;//how many pennies
+            
+            //Loop will continue to run until cannot remove 0.25 from amount
+            while (amount >= 0.25)
+            {
+                quarter += 1;
+                amount -= 0.25;
+            }
+            //Loop will continue to run until cannot remove 0.10 from amount
+            while (amount >= 0.10)
+            {
+                dime += 1;
+                amount -= 0.10;
+            }
+            //Loop will continue to run until cannot remove 0.05 from amount
+            while (amount <= 0.05)
+            {
+                nickle += 1;
+                amount -= 0.05;
+            }
+            //Loop will continue to run until cannot remove 0.01 from amount
+            while (amount > 0.01)
+            {
+                penney += 1;
+                amount -= 0.01;
+            }
+            Console.WriteLine("Amount: $" + amount);
+            Console.WriteLine("Quarters: " + quarter); ;
+            Console.WriteLine("Dimes: " + dime);
+            Console.WriteLine("Nickles: " + nickle);
+            Console.WriteLine("Pennies: " + penney);
+        }
+    }
+}
